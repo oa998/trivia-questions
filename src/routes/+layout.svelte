c0f7fbc035decd9a1559497e64221452968790db
zzz sss <onlineaccounts998@gmail.com>
2024-05-18 00:43:16 -0400

    auto commit

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,23 +1,7 @@
 <script lang="ts">
-  import { browser } from "$app/environment";
-  import { goto } from "$app/navigation";
-  import { getAllDays } from "$lib/index";
   import "../app.css";
-
-  let loading = true;
-  if (browser && location.pathname.startsWith("/songs")) {
-    loading = false;
-  } else if (browser) {
-    getAllDays()
-      .then(() => (loading = false))
-      .catch(() => goto("/"));
-  }
 </script>
 
-{#if loading}
-  <div>...loading</div>
-{:else}
-  <div style="background: #0d2a47;">
-    <slot />
-  </div>
-{/if}
+<div style="background: #0d2a47;">
+  <slot />
+</div>
